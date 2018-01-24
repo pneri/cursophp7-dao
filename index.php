2c4788253b161dabd4df638d2bc72850baec87e6
pneri <pcpneri@gmail.com>
2018-01-24 15:55:58 -0200

    Metodos de lista

--- index.php
+++ index.php
@@ -2,16 +2,34 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega o usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
 
-$root->loadbyId(3);
+echo $usuario;
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("j");
+//echo json_encode($search);
+
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+
+//Carrega uma lista de usuarios
+//$lista = usuario::getlist();
+//echo json_encode($lista);
+
+
+
 
 //$sql = new Sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
+
+
+
+
 
 ?>
